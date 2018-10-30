6f5ba4053dd629995d2592ae487aecc94b51862f
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-10-30 13:19:05 +0800

    update: created a new job request app without the _

---
+++ jobrequest/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import JobRequest
+
+
+class JobRequestProfile(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('date', 'due_date', 'total_hours_minutes_allocated')
+        }),
+        ('Job Request informations', {
+            'fields': (
+                'client_code',
+                'job_request_title',
+                'job_request_sent_via',
+                'job_request_instruction',
+                'project_managers',
+                'VA_admin_support',
+                'status_of_the_job_request',
+                'notes_and_coaching_from_project_manager'
+           )
+        })
+    )
+
+
+admin.site.register(JobRequest, JobRequestProfile)
--- jobrequest/models.py
+++ jobrequest/models.py
@@ -10,6 +10,7 @@ class JobRequest(models.Model):
         ('IN PROGRESS', 'In Progress'),
         ('COMPLETE', 'Complete')
     )
+
     date = models.DateField()
     due_date = models.DateField()
     client_code = models.CharField(max_length=250)
@@ -18,5 +19,6 @@ class JobRequest(models.Model):
     job_request_instruction = models.TextField()
     total_hours_minutes_allocated = models.DecimalField(max_digits=4, decimal_places=2)
     project_managers = models.ForeignKey('client.ProjectManager', default='Gillian', on_delete=models.PROTECT)
-    VA_admin_support = models.CharField(max_length=250, choices=STATUS_OF_JOB_REQUEST)
+    VA_admin_support = models.CharField(max_length=250)
+    status_of_the_job_request = models.CharField(max_length=250, default='----', choices=STATUS_OF_JOB_REQUEST)
     notes_and_coaching_from_project_manager = models.TextField()
