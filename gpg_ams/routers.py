857f44c9b9916eaac1fbdadcb45480a3c441fae2
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-10-31 18:41:25 +0800

    added: API for jobrequest

--- gpg_ams/routers.py
+++ gpg_ams/routers.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 from client.views import ClientViewSet
+from jobrequest.views import JobRequestViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register(r'client', ClientViewSet)
+router.register(r'jobrequest', JobRequestViewSet)
--- jobrequest/views.py
+++ jobrequest/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from .models import JobRequest
+from .serializers import JobRequestSerializer
+
+
+class JobRequestViewSet(viewsets.ModelViewSet):
+    queryset = JobRequest.objects.all()
+    serializer_class = JobRequestSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('client_code')
