02689ffaf6626d094e7ad902ef683e19481fff2d
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-11-01 16:57:22 +0800

    added: reporting app serializers and API

--- gpg_ams/routers.py
+++ gpg_ams/routers.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 from client.views import ClientViewSet
 from jobrequest.views import JobRequestViewSet
+from reporting.views import ReportingViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register(r'client', ClientViewSet)
 router.register(r'jobrequest', JobRequestViewSet)
+router.register(r'reporting', ReportingViewSet)
--- reporting/views.py
+++ reporting/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from .models import Report
+from .serializers import ReportingSerializer
+
+
+class ReportingViewSet(viewsets.ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportingSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title_of_job_request')
