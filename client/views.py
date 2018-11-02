b48edf53083d74f8fb773910ea28116bad261af1
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-11-02 22:01:26 +0800

    updated: client app views, added LoginRequiredMixin. Action tableheader in the senior tab

--- client/views.py
+++ client/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from rest_framework import viewsets, filters
 from .models import Client
 from .serializers import ClientSerializer
 
 
-class ClientView(View):
+class ClientView(LoginRequiredMixin, View):
     template_name = 'client/seniors_tab.html'
 
     def get(self, request):
