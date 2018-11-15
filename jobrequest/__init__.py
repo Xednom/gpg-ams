b52cf20e08b2507ea45612f3f827209fd8114b24
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-11-15 22:41:54 +0800

    added: verbose name in the apps

---
+++ jobrequest/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'jobrequest.apps.JobrequestConfig'
--- jobrequest/apps.py
+++ jobrequest/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class JobrequestConfig(AppConfig):
     name = 'jobrequest'
+    verbose_name = 'Job Request Information Database'
