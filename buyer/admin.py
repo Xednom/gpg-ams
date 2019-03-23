a9ba6a57264c49ffbeea123edf32d76edb10b62f
Monde <monde.lacanlalay@gmail.com>
2019-03-23 15:22:22 +0800

    wip: seller data for call me

--- buyer/admin.py
+++ buyer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CustomerCareSpecialist, CygnusInvestment
+from .models import CustomerCareSpecialist, CygnusInvestment, WeBuyAcreageLlc
 
 
 class CygnusInvestmentProfile(admin.ModelAdmin):
@@ -27,5 +27,31 @@ class CygnusInvestmentProfile(admin.ModelAdmin):
         }),
     )
 
+class WeBuyAcreageLlcProfile(admin.ModelAdmin):
+    list_display = ('call_date', 'customer_care_specialist',
+                    'name_of_the_caller', 'phone_number_of_caller')
+    list_filter = ('customer_care_specialist',)
+    search_fields = ('customer_care_specialist__name', 'are_you_an_agent')
+    fieldsets = (
+        ('Buyer form informations', {
+            'fields': (
+                'call_date',
+                'customer_care_specialist',
+                'contact_information',
+                'address_or_apn',
+                'name_of_the_caller',
+                'phone_number_of_caller',
+                'are_you_an_agent',
+                'listing',
+                'your_phone_number',
+                'email_address',
+                'average_handling_time',
+                'additional_notes',
+            )
+        }),
+    )
+
+
 admin.site.register(CustomerCareSpecialist)
 admin.site.register(CygnusInvestment, CygnusInvestmentProfile)
+admin.site.register(WeBuyAcreageLlc, WeBuyAcreageLlcProfile)
