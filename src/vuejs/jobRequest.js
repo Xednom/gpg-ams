216aa3c3ac9390d4ef5748ec86f416a536b08eec
Monde <monde.lacanlalay@gmail.com>
2019-04-12 14:25:16 +0800

    updated: added pagination and logic when saving(loading)

--- src/vuejs/jobRequest.js
+++ src/vuejs/jobRequest.js
@@ -37,7 +37,14 @@ new Vue({
       'company_billable_to': null,
       'company_asigned_to': null,
     },
-    search_term: ''
+    search_term: '',
+    // for pagination
+    currentPage: 1,
+    pageSize: RECORDS_PER_PAGE,
+    startPage: 1,
+    endPage: null,
+    maxPages: RECORDS_PER_PAGE,
+    paginatedRecords: [],
   },
   mounted: function() {
     this.getJobRequests();
@@ -213,6 +220,70 @@ new Vue({
         }
       });
 
-    }
-  }
+    },
+    getPaginatedRecords: function () {
+        const startIndex = this.startIndex;
+        this.paginatedRecords = this.jobRequests.slice().splice(startIndex, this.pageSize);
+      },
+      goToPage: function (page) {
+        if (page < 1) {
+          return this.currentPage = 1;
+        }
+        if (page > this.totalPages) {
+          return this.currentPage = this.totalPages;
+        }
+        this.currentPage = page;
+      },
+      setPageGroup: function () {
+        if (this.totalPages <= this.maxPages) {
+          this.startPage = 1;
+          this.endPage = Math.min(this.totalPages, this.maxPages);
+        } else {
+          let maxPagesBeforeCurrentPage = Math.floor(this.maxPages / 2);
+          let maxPagesAfterCurrentPage = Math.ceil(this.maxPages / 2) - 1;
+          if (this.currentPage <= maxPagesBeforeCurrentPage) {
+            // current page near the start
+            this.startPage = 1;
+            this.endPage = this.maxPages;
+          } else if (this.currentPage + maxPagesAfterCurrentPage >= this.totalPages) {
+            // current page near the end
+            this.startPage = this.totalPages - this.maxPages + 1;
+            this.endPage = this.totalPages;
+          } else {
+            // current page somewhere in the middle
+            this.startPage = this.currentPage - maxPagesBeforeCurrentPage;
+            this.endPage = this.currentPage + maxPagesAfterCurrentPage;
+          }
+        }
+      }
+  },
+  watch: {
+      jobRequests: function (newjobRequestsRecords, oldjobRequestsRecords) {
+        this.setPageGroup();
+        this.getPaginatedRecords();
+      },
+      currentPage: function (newCurrentPage, oldCurrentPage) {
+        this.setPageGroup();
+        this.getPaginatedRecords()
+      },
+    },
+    computed: {
+      totalItems: function () {
+        return this.jobRequests.length;
+      },
+      totalPages: function () {
+        return Math.ceil(this.totalItems / this.pageSize);
+      },
+      startIndex: function () {
+        return (this.currentPage - 1) * this.pageSize;
+      },
+      endIndex: function () {
+        return Math.min(this.startIndex + this.pageSize - 1, this.totalItems - 1);
+      },
+      pages: function () {
+        let pages = [];
+        for (let i = this.startPage; i <= this.endPage; i++) pages.push(i);
+        return pages;
+      },
+    },
 });
