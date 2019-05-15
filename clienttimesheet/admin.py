0b16b07fab17a73718503d01073fac477a517f98
Monde <monde.lacanlalay@gmail.com>
2019-05-15 13:13:10 +0800

    added total for admin in timesheet

--- clienttimesheet/admin.py
+++ clienttimesheet/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from admin_totals.admin import ModelAdminTotals
+
+from django.db.models import Sum
 
 from . models import TimeSheet, PaymentMade
 
 
-class TimeSheetProfile(ImportExportModelAdmin):
+class TimeSheetProfile(ModelAdminTotals):
     list_display = ('company_tagging', 'shift_date', 'month_to_date', 'clients_full_name',
                     'title_job_request', 'time_in', 'time_out', 'duration', 'hourly_rate',
                     'total_tax_fee', 'total_amount_due')
     list_filter = ('company_tagging', 'shift_date', 'clients_full_name')
+    list_totals = [('total_tax_fee', Sum), ('total_amount_due', Sum), ('duration', Sum)]
     search_fields = ('company_tagging', 'clients_full_name', 'shift_date', 'month_to_date',
                      'title_job_request')
     readonly_fields = ('total_tax_fee', 'total_amount_due', 'amount_charge', 'duration')
@@ -37,9 +41,11 @@ class TimeSheetProfile(ImportExportModelAdmin):
     )
 
 
-class PaymentMadeProfile(ImportExportModelAdmin):
-    list_display = ('date', 'client_name', 'amount', 'reference', 'payment_channel')
+class PaymentMadeProfile(ModelAdminTotals):
+    list_display = ('date', 'client_name', 'reference',
+                    'payment_channel', 'amount',)
     list_filter = ('date', 'reference', 'payment_channel')
+    list_totals = [('amount', Sum)]
     search_fields = ('reference', 'payment_channel')
     fieldsets = (
         ("Payment Made for the Client", {
