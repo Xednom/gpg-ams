a3f457cbcc1204c64bd1436030aa00c295b92fa4
Monde <monde.lacanlalay@gmail.com>
2019-05-16 18:25:49 +0800

    added time sheets in due diligencce

--- users/admin.py
+++ users/admin.py
@@ -26,7 +26,7 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['username']
     UserAdmin.fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name','notes', 'is_staffs','is_client')}),
+        (_('Personal info'), {'fields': ('notes', 'is_staffs','is_client')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),)
