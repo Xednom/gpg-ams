4269f2e6362635faff97ac2c2c8aeba3f2d21958
Monde <monde.lacanlalay@gmail.com>
2019-06-10 12:04:46 +0800

    fixed date dropdowns on dd tracker

---
+++ landmaster/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import DueDiligencesCleared
+
+
+class DueDiligenceCallCreateForm(forms.Form):
+    date_of_call = forms.DateField(input_formats=['%d/%m/%Y'])
+    call_in = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+    call_out = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
