d5d2547b67de6e8498e7771b6566df11f50a37ee
Monde <monde.lacanlalay@gmail.com>
2019-06-18 16:07:28 +0800

    fixed the queries in reminders and removed seller app infos

--- seller/serializers.py
+++ seller/serializers.py
@@ -5,21 +5,3 @@ from rest_framework import serializers
 
 from .models import AffordableLandInvestment, FranklinManagement
 from buyer.models import CustomerCareSpecialist
-
-
-class AffordableLandSerializer(serializers.ModelSerializer):
-    customer_care_specialist = serializers.SlugRelatedField(slug_field='name', queryset=CustomerCareSpecialist.objects.all())
-    call_date = serializers.DateField(format="%Y-%m-%d")
-
-    class Meta:
-        model = AffordableLandInvestment
-        fields = '__all__'
-
-
-class FranklinSerializer(serializers.ModelSerializer):
-    customer_care_specialist = serializers.SlugRelatedField(slug_field='name', queryset=CustomerCareSpecialist.objects.all())
-    call_date = serializers.DateField(format="%Y-%m-%d")
-
-    class Meta:
-        model = FranklinManagement
-        fields = '__all__'
--- seller/urls.py
+++ seller/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name="seller"
 
 urlpatterns = [
-    path('affordable-land-investment/', views.AffordableLandView.as_view(), name='affordable-land'),
-    path('franklin-management/', views.FranklinManagementView.as_view(), name='franklin-management')
+    
 ]
