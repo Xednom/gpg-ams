52212ff70411403039c07450c4dfa7da9b70257c
Monde <monde.lacanlalay@gmail.com>
2019-06-22 15:10:57 +0800

    fixed the lookups for Due Diligence

--- landmaster/serializers.py
+++ landmaster/serializers.py
@@ -5,6 +5,8 @@ from .models import DueDiligence, DueDiligencesCleared
 
 from fillables.models import CompanyName, VirtualAssistant, ProjectManager
 
+from users.models import Staffs
+
 
 class VaSerializer(serializers.ModelSerializer):
     
@@ -15,15 +17,15 @@ class VaSerializer(serializers.ModelSerializer):
 
 class DueDiligenceSerializer(serializers.ModelSerializer):
     dd_va_assigned_initial_data = serializers.SlugRelatedField(
-        slug_field='name', queryset=VirtualAssistant.objects.all(), allow_null=True, required=False)
+        slug_field='full_name', queryset=Staffs.objects.all(), allow_null=True, required=False)
     dd_va_assigned_call_outs_tax_data = serializers.SlugRelatedField(
-        slug_field='name', queryset=VirtualAssistant.objects.all(), allow_null=True, required=False)
+        slug_field='full_name', queryset=Staffs.objects.all(), allow_null=True, required=False)
     dd_va_assigned_call_outs_zoning_data = serializers.SlugRelatedField(
-        slug_field='name', queryset=VirtualAssistant.objects.all(), allow_null=True, required=False)
+        slug_field='full_name', queryset=Staffs.objects.all(), allow_null=True, required=False)
     dd_va_assigned_call_outs_utilities_data = serializers.SlugRelatedField(
-        slug_field='name', queryset=VirtualAssistant.objects.all(), allow_null=True, required=False)
+        slug_field='full_name', queryset=Staffs.objects.all(), allow_null=True, required=False)
     dd_va_assigned_call_outs_other_requests = serializers.SlugRelatedField(
-        slug_field='name', queryset=VirtualAssistant.objects.all(), allow_null=True, required=False)
+        slug_field='full_name', queryset=Staffs.objects.all(), allow_null=True, required=False)
     project_manager = serializers.SlugRelatedField(slug_field='project_manager', queryset=ProjectManager.objects.all(), allow_null=True, required=False)
 
     class Meta:
