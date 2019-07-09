317acbc49bce876b60597f68e754dc09c6b3ca0a
Monde <monde.lacanlalay@gmail.com>
2019-07-09 19:33:06 +0800

    fixed isse when searching in admin in callme inventory, added sum and from and to dates filtering

--- callmeinventory/admin.py
+++ callmeinventory/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from jet.filters import DateRangeFilter
+from django.db.models import Sum, Avg
+from admin_totals.admin import ModelAdminTotals
+
 from .models import inventory
 
 
-class InventoryProfile(admin.ModelAdmin):
+class InventoryProfile(ModelAdminTotals):
     list_display = ('date_lead_received', 'type_of_form',
                     'client_full_name', 'client_company_name',
                     'full_name_of_lead', 'phone_number', 'status', 'financial_status',
                     'call_duration', 'total_time_transferring_leads', 'total_mins')
-    list_filter = ('type_of_form', 'status', 'financial_status')
+    list_filter = ('type_of_form', 'status', 'financial_status',
+                   ('transferred_date', DateRangeFilter),
+                   ('date_lead_received', DateRangeFilter))
+    list_totals = [('call_duration', Sum), ('total_time_transferring_leads', Sum),
+                   ('total_mins', Sum)]
     search_fields = ('client_full_name', 'client_company_name',
                      'customer_representative', 'type_of_form', 'status', 
-                     'financial_status', ('transferred_date', DateRangeFilter),
-                     ('date_lead_received', DateRangeFilter))
+                     'financial_status')
     readonly_fields = ('total_mins',)
     fieldsets = (
         ("Call Me Inventory Informations", {
