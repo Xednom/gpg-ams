72284c4b5f10a0b8b688dcdb989ac9ef84f63493
Monde <monde.lacanlalay@gmail.com>
2019-08-03 17:10:47 +0800

    added researcher name in O2O add and view page; changed name for call out reports in base.

--- landacademy/serializers.py
+++ landacademy/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import O20SmartPricing, LandAcademyInventory
+from users.models import Staffs
 
 
 class LandAcademySerializer(serializers.ModelSerializer):
@@ -11,6 +12,10 @@ class LandAcademySerializer(serializers.ModelSerializer):
 
 
 class SmartPricingSerializer(serializers.ModelSerializer):
+    researcher_name = serializers.SlugRelatedField(slug_field="full_name", queryset=Staffs.objects.all(), \
+                                                   allow_null=True, required=False)
+    quality_specialist = serializers.SlugRelatedField(slug_field="full_name", queryset=Staffs.objects.all(), \
+                                                      allow_null=True, required=False)
 
     class Meta:
         model = O20SmartPricing
