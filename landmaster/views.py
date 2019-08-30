0c42ccd52f77a15549200ee1819e554b1fbba2e4
Monde <monde.lacanlalay@gmail.com>
2019-08-30 19:28:29 +0800

    fixed the query for DD

--- landmaster/views.py
+++ landmaster/views.py
@@ -75,23 +75,23 @@ class DueDiligenceViewSet(viewsets.ModelViewSet):
         elif is_staff:
             if self.request.user.staffs.position == "Project Manager":
                 queryset = due_diligence.filter(
-                    Q(project_manager__full_name__icontains=self.request.user.staffs.full_name), \
-                    Q(status_of_dd="Sent to Project Manager") | \
-                    Q(status_of_dd="Project Managers Review") | \
-                    Q(status_of_dd="Sent to VA") | \
-                    Q(status_of_dd="VA Processing") | \
+                    Q(project_manager__full_name__icontains=self.request.user.staffs.full_name), 
+                    Q(status_of_dd="Sent to Project Manager") | 
+                    Q(status_of_dd="Project Managers Review") | 
+                    Q(status_of_dd="Sent to VA") | 
+                    Q(status_of_dd="VA Processing") | 
                     Q(status_of_dd="Sent to Quality Specialist"))
                 return queryset
             elif self.request.user.staffs.position == "General Administrative Support":
-                queryset = due_diligence.filter(Q(dd_va_assigned_initial_data__full_name__icontains=self.request.user.staffs.full_name) | \
-                    Q(dd_va_assigned_call_outs_tax_data__full_name__icontains=self.request.user.staffs.full_name) | \
-                    Q(dd_va_assigned_call_outs_zoning_data__full_name__icontains=self.request.user.staffs.full_name) | \
-                    Q(dd_va_assigned_call_outs_utilities_data__full_name__icontains=self.request.user.staffs.full_name) | \
-                    Q(dd_va_assigned_call_outs_other_requests__full_name__icontains=self.request.user.staffs.full_name) | \
-                    Q(status_of_dd="Sent to Project Manager") | \
-                    Q(status_of_dd="Project Managers Review") | \
-                    Q(status_of_dd="Sent to VA") | \
-                    Q(status_of_dd="VA Processing") | \
+                queryset = due_diligence.filter(Q(dd_va_assigned_initial_data__full_name__icontains=self.request.user.staffs.full_name),
+                    Q(dd_va_assigned_call_outs_tax_data__full_name__icontains=self.request.user.staffs.full_name), 
+                    Q(dd_va_assigned_call_outs_zoning_data__full_name__icontains=self.request.user.staffs.full_name), 
+                    Q(dd_va_assigned_call_outs_utilities_data__full_name__icontains=self.request.user.staffs.full_name), 
+                    Q(dd_va_assigned_call_outs_other_requests__full_name__icontains=self.request.user.staffs.full_name), 
+                    Q(status_of_dd="Sent to Project Manager") | 
+                    Q(status_of_dd="Project Managers Review") | 
+                    Q(status_of_dd="Sent to VA") | 
+                    Q(status_of_dd="VA Processing") | 
                     Q(status_of_dd="Sent to Quality Specialist"))
                 return queryset
 
