929913367374670514c75d397c203709766b2aea
Xednom <monde.lacanlalay@gmail.com>
2020-07-16 14:48:08 +0800

    removed 404 in users url

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,3 @@ urlpatterns = [
     path('json-chart/client_status_data/', views.client_status_data, name='client_status_data'),
     path('json-chart/job_request_data/', views.job_request_status_data, name='job_request_status_data'),
 ]
-
-handler404 = 'views.error_404_view'
